d5e509abb04e6c3f9055f9897c032351632dbb2b
fushime2 <okmt52@gmail.com>
2017-01-22 11:42:41 +0900

    スターズに対応

--- getcsv.py
+++ getcsv.py
@@ -1,64 +1,77 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import requests
-import csv
 import re
+import urllib.request
+import csv
 from bs4 import BeautifulSoup
 
-def get_last_page(s):
-    pos_f = s.find('p=')
-    pos_e = s.find('&amp')
-    if pos_f == -1:
-        return '1'
+# page = 1,2,...
+# prefnum = 01,02,...,47
+BASE_URL = "http://www.aikatsu.com/stars/playshop/list.php?p={page:d}&pref={prefnum:02d}"
+
+rows = [["店名", "住所"]]
+
+
+def get_last_page(pref):
+    """ '>>' の href 要素から最後のページ番号を取得 """
+    url = BASE_URL.format(page=1, prefnum=pref)
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, "lxml")
+    try:
+        href = soup.find("a", text=re.compile(("»")))["href"]
+    except KeyError:
+        return 1
+    last_page = re.search("p=[0-9]+&", href).group().replace("p=", "").replace("&", "")
+    return int(last_page)
+
+
+def format_string(tag):
+    """ タグから空白とかを削除して文字列型で返す """
+    delete = [" ", "\n", "\r", "<td>"]
+    if tag.string is not None:
+        s = tag.string
+        for d in delete:
+            s = s.replace(d, "")
+        print(s)
+        return s
     else:
-        return s[pos_f+2 : pos_e]
+        s = str(tag)
+        ng_words = ["paginator", "titlenum"]
+        for ng in ng_words:
+            if ng in s: return
+        p = s.find("<a class")
+        s = s[:p]
+        for d in delete:
+            s = s.replace(d, "")
+        print(s)
+        return s
+
+
+def make_list(pref, page):
+    url = BASE_URL.format(page=page, prefnum=pref)
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, "lxml")
+    td = soup.find_all("td")
+    data = list()
+    for tag in td:
+        str = format_string(tag)
+        if str is not None:
+            data.append(str)
+    for i in range(0, len(data), 2):
+        rows.append([data[i], data[i + 1]])
+
 
 def main():
-    """Create csv file"""
-    PREFURL = 'http://www.aikatsu.com/shop/list.php?p={}&pref={:02d}'
-    with open('shops.csv', 'w') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(['店名', '住所'])
-        for pref_num in xrange(47):
-            first_page = PREFURL.format(1, pref_num+1)
-            r = requests.get(first_page)
-            soup = BeautifulSoup(r.text.encode(r.encoding))
-            s = soup.find(class_='mark last page-numbers')
-            last_page_num = get_last_page(str(s))
-            for page in xrange(int(last_page_num)):
-                url = PREFURL.format(page+1, pref_num+1)
-                r = requests.get(url)
-                soup = BeautifulSoup(r.text.encode(r.encoding))
-                s = soup.find("td", class_="")
-                store_name = s.next_element
-                store_name = store_name.encode('utf-8')
-                print store_name
-                s = s.find_next("td", class_="")
-                address = s.next_element
-                address = re.sub(r'	', '', address)
-                address = address.encode('utf-8')
-                print address
-                s = s.find_next("td", class_="")
-                writer.writerow([store_name, address])
+    print("Start")
+    for pref in range(1, 48):
+        lastpage = get_last_page(pref)
+        for page in range(1, lastpage + 1):
+            make_list(pref, page)
 
-                for i in xrange(9):
-                    try:
-                        store_name = s.find_next("td", class_="").next_element
-                        store_name = store_name.encode('utf-8')
-                        s = s.find_next("td", class_="")
-                        address = s.find_next("td", class_="").next_element
-                        address = re.sub(r'	', '', address)
-                        address = address.encode('utf-8')
-                        print store_name
-                        print address
-                        writer.writerow([store_name, address])
-                        for j in xrange(2):
-                            s = s.find_next("td", class_="")
-                        s.find_next("td", class_="")
-                    except:
-                        break
+    f = open("shops.csv", "w", encoding="utf-8")
+    writer = csv.writer(f, lineterminator="\n")
+    writer.writerows(rows)
+    f.close()
+    print("End")
 
-    print 'ok ok okke-'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
